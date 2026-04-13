7292f1b04be6e9680616477baa6a880d7adf4037
@Sive3908653 <hoyosive558@gmail.com>
2026-04-13 18:27:15 +0200

    First Commit

---
+++ data.py
@@ -0,0 +1,29 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body' : "In examination-oriented higher education systems, academic performance functions not only as anIn examination-oriented higher education systems, academic performance functions not only as an",
+            'Author': 'Sive Hoyo',
+            'create_date' : '16-03-2026'
+
+    },
+    {
+            'id': 2,
+            'title': 'Article Two',
+            'body' : "In examination-oriented higher education systems, academic performance functions not only as anIn examination-oriented higher education systems, academic performance functions not only as an",
+            'Author': 'Anga Xatasi',
+            'create_date' : '16-03-2026'
+
+    },
+    {
+            'id': 3,
+            'title': 'Article Three',
+            'body' : "In examination-oriented higher education systems, academic performance functions not only as anIn examination-oriented higher education systems, academic performance functions not only as an",
+            'Author': 'Mziyanda Vena',
+            'create_date' : '16-03-2026'
+
+    }
+   
+    ]
+    return articles
